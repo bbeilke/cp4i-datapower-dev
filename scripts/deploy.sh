3ec3284c9c2c61ae64ed24ea39463f0f7244c761
peterajessup <peterajessup@users.noreply.github.com>
2021-09-23 09:04:05 +1000

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,10 @@ set +e
 oc delete DataPowerService dev1
 oc delete Service dev1-service
 oc delete Route dproute
+oc delete Secret admin-credentials
 set +e 
 
+oc create secret generic admin-credentials --from-literal=password=password
 oc create configmap foobar-config --from-file=foobar.cfg
 oc apply -f dev1-dp-service.yaml
 oc apply -f dev1-dp-route.yaml
